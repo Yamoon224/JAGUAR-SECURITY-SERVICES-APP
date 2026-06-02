f7e2bb54eb9099fe2c7a8d99198bee51d24d5fc3
Yamoon224 <Yamooon664@gmail.com>
2026-06-02 15:24:17 +0000

    Add English and French authentication language files with error messages

---
+++ resources/lang/fr/auth.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'failed'   => 'Identifiants incorrects.',
+    'password' => 'Mot de passe incorrect.',
+    'throttle' => 'Trop de tentatives de connexion. Veuillez réessayer dans :seconds secondes.',
+];
