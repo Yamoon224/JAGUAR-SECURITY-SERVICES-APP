20c42070b163d8ee4574c1639e13bbe7921472c5
Jamusza <160267741+Yamoon224@users.noreply.github.com>
2026-07-01 09:37:14 +0000

    Refactor signature image handling in PrintController
    Comment out the existing signature image handling and adjust the position for the new image handling.

--- app/Http/Controllers/PrintController.php
+++ app/Http/Controllers/PrintController.php
@@ -456,9 +456,9 @@ class PrintController extends Controller
         self::$obj->SetX(120);
         self::$obj->Cell(80, 5, utf8_decode('Conakry, le ' . date('d/m/Y')), 0, 1, 'C');
         $signY = self::$obj->GetY() + 2;
-        if (file_exists('images/signature.png')) {
+        /* if (file_exists('images/signature.png')) {
             self::$obj->Image('images/signature.png', 132, $signY, 30, 0);
-        }
+        } */
         if (file_exists('images/signature.png')) {
             self::$obj->Image('images/signature.png', 138, $signY + 4, 46, 0);
         }
